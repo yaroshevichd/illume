7962194d78d7493bb3841ad234564c3b95d2ee85
Denis Yaroshevich <d.yaroshevich@itransition.com>
2015-11-13 16:01:46 +0300

    Improve negative tests naming

--- IllumeUtils_example/IllumeUtils_example.ino
+++ IllumeUtils_example/IllumeUtils_example.ino
@@ -39,18 +39,16 @@ struct cmd_test_parsed_if_one_args_passed : TestOnce
 } cmd_test_parsed_if_one_args_passed_instance;
 
 
-struct cmd_test_check_failed : TestOnce
+struct CmdTestNegativeTestOnce : TestOnce
 {
-	String m_testString;
 	struct Command m_actualResult;
 
-	cmd_test_check_failed(const char * test_name)
+	CmdTestNegativeTestOnce(const char * test_name)
 		: TestOnce(test_name)
 	{}
 
 	void once()
 	{
-		StringStream stream(m_testString);
 		assertEqual(m_actualResult.m_type, CommandType_None);
 		assertEqual(m_actualResult.m_argc, -1);
 		assertTrue(m_actualResult.m_argv == NULL);
@@ -58,34 +56,34 @@ struct cmd_test_check_failed : TestOnce
 };
 
 
-struct cmd_test_is_parsed_as_none_if_args_more_than_count : cmd_test_check_failed
+struct cmd_test_is_parsed_as_none_if_args_more_than_count : CmdTestNegativeTestOnce
 {
 	cmd_test_is_parsed_as_none_if_args_more_than_count()
-		: cmd_test_check_failed("cmd_test_is_parsed_as_none_if_args_more_than_count")
+		: CmdTestNegativeTestOnce("cmd_test_is_parsed_as_none_if_args_more_than_count")
 	{}
 
 	void once()
 	{
-		m_testString = String("$test,abc,bca");
-		StringStream test_stream(m_testString);
+		String test_string("$test,abc,bca");
+		StringStream test_stream(test_string);
 		m_actualResult = readCommand(test_stream);
-		cmd_test_check_failed::once();
+		CmdTestNegativeTestOnce::once();
 	}
 } cmd_test_is_parsed_as_none_if_args_more_than_count_instance;
 
 
-struct cmd_test_is_parsed_as_none_if_args_less_than_count : cmd_test_check_failed
+struct cmd_test_is_parsed_as_none_if_args_less_than_count : CmdTestNegativeTestOnce
 {
 	cmd_test_is_parsed_as_none_if_args_less_than_count()
-		: cmd_test_check_failed("cmd_test_is_parsed_as_none_if_args_less_than_count")
+		: CmdTestNegativeTestOnce("cmd_test_is_parsed_as_none_if_args_less_than_count")
 	{}
 
 	void once()
 	{
-		m_testString = String("$test,2,0|0");
-		StringStream test_stream(m_testString);
+		String test_string("$test,2,0|0");
+		StringStream test_stream(test_string);
 		m_actualResult = readCommand(test_stream);
-		cmd_test_check_failed::once();
+		CmdTestNegativeTestOnce::once();
 	}
 } cmd_test_is_parsed_as_none_if_args_less_than_count_instance;
 
